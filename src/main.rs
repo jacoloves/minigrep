3e64d6bdc73a7fa04b87c8d8834b0a6be943b8f2
jacoloves <5511068t@gmail.com>
2023-01-11 17:51:55 +0900

    unwrap_or_else add

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use std::{
     env::{self, args},
     fs::File,
     io::Read,
+    process,
 };
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -27,13 +31,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
