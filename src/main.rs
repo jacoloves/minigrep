0c78e6968e4f625a25b3bc76fb49feee3b435b65
jacoloves <5511068t@gmail.com>
2023-01-12 18:31:01 +0900

    Error process add

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     env::{self, args},
+    error::Error,
     fs::File,
     io::Read,
     process,
@@ -16,13 +17,22 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went erong reading the file");
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
